a47a87c20e363b286d8ae02a35ddf499e05be10e
dayannearaujo <dayanne.araujo8@gmail.com>
2020-04-22 00:57:37 -0400

    typo fix

--- covid19ca/settings.py
+++ covid19ca/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['covid19-ca.herokuapp.com/']
+ALLOWED_HOSTS = ['covid19-ca.herokuapp.com']
 
 
 # Application definition
