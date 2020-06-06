187c59beae33eb5041ccdbf6d5664084084791de
dayannearaujo <dayanne.araujo8@gmail.com>
2020-06-06 19:48:21 -0400

    Adding django_filter lib

--- covid19ca/settings.py
+++ covid19ca/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'django_extensions',
@@ -131,3 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
