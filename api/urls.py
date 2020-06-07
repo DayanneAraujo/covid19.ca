3f13d9b04bb2b5c69716f6d4550ec91298eda363
dayannearaujo <dayanne.araujo8@gmail.com>
2020-06-06 20:15:15 -0400

    Add code changes and debug flag

--- api/urls.py
+++ api/urls.py
@@ -3,25 +3,21 @@ from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
+root_url = "domain"
+router.get_api_root_view().cls.__doc__ = \
+    "You can filter by provinces making requests like:" \
+    f'\n - http://{root_url}/api/canada/?prname=Ontario' \
+    f'\n - http://{root_url}/api/canada/?prname=British Columbia' \
+    "\n " \
+    "\n Available provinces are: " \
+    "['Ontario', 'British Columbia', 'Canada', 'Quebec', 'Alberta', \
+       'Repatriated Travellers', 'Saskatchewan', 'Manitoba', \
+       'New Brunswick', 'Newfoundland and Labrador', \
+       'Prince Edward Island', 'Nova Scotia', 'Northwest Territories', \
+       'Nunavut', 'Yukon', 'Repatriated travellers']" \
+
+
 router.register('canada', CanadaViewSet)
-router.register('ontario', OntarioViewSet, basename='ontario')
-router.register('british-columbia', BritishColumbiaViewSet,
-                basename='british-columbia')
-router.register('quebec', QuebecViewSet, basename='quebec')
-router.register('alberta', AlbertaViewSet, basename='alberta')
-router.register('saskatchewan', SaskatchewanViewSet, basename='saskatchewan')
-router.register('manitoba', ManitobaViewSet, basename='manitoba')
-router.register('newfoundland-and-labrador', NewfoundlandAndLabradorViewSet,
-                basename='newfoundland-and-labrador')
-router.register('prince-edward-island', PrinceEdwardIslandViewSet,
-                basename='prince-edward-island')
-router.register('nova-scotia', NovaScotiaViewSet, basename='nova-scotia')
-router.register('northwest-territories', NorthwestTerritoriesViewSet,
-                basename='northwest-territories')
-router.register('nunavut', NunavutViewSet, basename='nunavut')
-router.register('yukon', YukonViewSet, basename='yukon')
-router.register('repatriated-travellers', RepatriatedTravellersViewSet,
-                basename='repatriated-travellers')
 
 urlpatterns = [
     path('', include(router.urls))
